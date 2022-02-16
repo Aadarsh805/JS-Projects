8ea1a48de6a5ea51d568c986bcf595a39a0cbc14
Aadarsh Thakur <aadarshthakur80501@gmail.com>
2022-02-16 13:38:39 +0530

    update

--- split landing page/script.js
+++ split landing page/script.js
@@ -6,6 +6,7 @@ checkBoxes();
 
 function checkBoxes(){
     const triggerBottom = window.innerHeight / 5 * 4;
+    
 
     boxes.forEach(box => {
         const boxTop = box.getBoundingClientRect().top;
