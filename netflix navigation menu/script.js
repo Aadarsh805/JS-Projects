1769eb8b9c915df18dc3893bd9df856dc3c7cc92
Aadarsh Thakur <aadarshthakur80501@gmail.com>
2022-02-27 10:18:55 +0530

    netflix navigation menu effect

---
+++ netflix navigation menu/script.js
@@ -0,0 +1,11 @@
+const openBtn = document.querySelector('.open-btn')
+const closeBtn = document.querySelector('.close-btn')
+const nav = document.querySelectorAll('.nav')
+
+openBtn.addEventListener('click', () => {
+    nav.forEach(navEl => navEl.classList.add('visible'))
+})
+
+closeBtn.addEventListener('click', () => {
+    nav.forEach(navEl => navEl.classList.remove('visible'))
+})
